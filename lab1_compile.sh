b34d64eaa8a503f67767d4d23eb07493e69b1035
kahanalan <kahanalan@gmail.com>
2023-03-28 09:54:21 -0300

    Agrego lab1_compile.sh para compilar y correr una sola vez

---
+++ lab1_compile.sh
@@ -0,0 +1,43 @@
+#!/bin/bash 
+
+export demo_name=demo
+export headless_name=headless
+
+Nruns=1 # Cantidad de ejecuciones para hacer los promedios de los ns_per_cell
+
+#TODO faltaría que el programa guarde algunos datos para después comparar (datos de salida para tests, y datos de metricas para comparar)
+
+for i in "gcc" ; do 
+	for j in "-O0"; do
+		
+		#Exporto los nombres de los compiladores y las opciones
+		export CC=$i
+		export CFLAGS="-std=c17 -Wall -Wextra ""$j"	#TODO Esto esta medio choto, como está escrito no hay que cambiar los espacios entre el ""
+
+		echo ''
+		echo ''
+		echo '------------'
+		echo $CC $CFLAGS
+
+		# Compilo
+		make clean
+		make
+
+		# Ejecuto headless
+		for n in $(eval echo "{1.."$Nruns"..1}"); do
+			# rm ....... # Borro archivos de datos anteriores
+			./${headless_name}
+			echo ''
+		done
+		
+		
+	done
+	
+done
+
+
+
+
+
+
+
