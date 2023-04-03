11eaae74eb8ab119473eec9d1ce4144c96d0e5fd
Nahuel Borda <nborda@alumnos.unc.edu.ar>
2023-04-03 14:36:31 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,5 +7,5 @@ export CFLAGS=$2
 make clean >/dev/null 2>&1
 make all >/dev/null 2>&1
 
-perf stat -x ';' -o output.txt -e cpu-clock,task-clock,context-switches,page-faults,cycles,instructions,branches,faults,migrations,duration_time ./headless >/dev/null 2>&1
+perf stat -r 10 -x ';' -o output.txt -e cpu-clock,task-clock,context-switches,page-faults,cycles,instructions,branches,faults,migrations,duration_time ./headless >/dev/null 2>&1
 cat output.txt
