2151a9ceaff492622cc6b5d741235ac9f0741264
CP2023 <cp202310@debian-BULLSEYE-live-builder-AMD64>
2023-04-03 23:25:20 -0300

    cambios en archivo de graficos

--- plots_lab1_pruebas_compiladores_2.py
+++ plots_lab1_pruebas_compiladores_2.py
@@ -12,7 +12,7 @@ legend=[]
 
 
 lista1=['gcc','icx','clang']
-lista2=["-O0", "-O1", "-O2", "-O3", "-Ofast", "-Os"]
+lista2=["-O0", "-Ofast", "-march=native"]
 #lista2=["-O0", "-O1"]
 
 
@@ -51,7 +51,7 @@ for i in lista1:
 
 	## defino las leyendas
 	plt.title(str(i))
-	legend=lista2
+	legend=["-O0", "-Ofast", "-Ofast -march=native"]
 	labels = legend
 	handles = [plt.Rectangle((0,0),1,1, color=clr) for clr in colores]
 	plt.legend(handles, labels)
@@ -60,6 +60,6 @@ for i in lista1:
 	#plt.show()
 
 	# guarda
-	filename="lab1_prueba_comps_v1_codigo_original_"+str(i)+".png"
+	filename="lab1_prueba_comps_v2_codigo_original_"+str(i)+".png"
 	plt.savefig(filename, bbox_inches='tight')
 	plt.close()
