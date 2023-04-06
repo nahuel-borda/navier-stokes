8f14982bff2ac0e5b45a5f8da750f39288d2a62b
alan_kahan <kahanalan@gmail.com>
2023-04-05 23:59:21 -0300

    cambio comp

--- lab1_compile.sh
+++ lab1_compile.sh
@@ -7,7 +7,7 @@ Nruns=1 # Cantidad de ejecuciones para hacer los promedios de los ns_per_cell
 
 #TODO faltaría que el programa guarde algunos datos para después comparar (datos de salida para tests, y datos de metricas para comparar)
 
-for i in "icx" ; do 
+for i in "gcc" ; do 
 	for j in ""; do
 		
 		#Exporto los nombres de los compiladores y las opciones
