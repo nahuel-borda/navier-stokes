b455090f36d04e3db9e2222b6adbd9945fe8664e
Nahuel B <you@example.com>
2023-04-06 18:04:08 -0300

    include internal metrics to data exported

--- run.sh
+++ run.sh
@@ -5,7 +5,10 @@ export CC=$1
 export CFLAGS=$2
 
 make clean >/dev/null 2>&1
+rm ./headless.dat 
+
 make all >/dev/null 2>&1
 
-perf stat -x ';' -o output.txt -e cpu-clock,task-clock,context-switches,page-faults,cycles,instructions,branches,faults,migrations,duration_time ./headless >/dev/null 2>&1
+perf stat -x ';' -o output.txt -e cpu-clock,task-clock,context-switches,page-faults,cycles,instructions,branches,faults,migrations,duration_time,cache-misses ./headless >/dev/null 2>&1
+python3 mean_outputs.py output.txt
 cat output.txt
