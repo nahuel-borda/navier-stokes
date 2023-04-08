826249a487e6a791ef055c88535301b5373b3b9c
alan_kahan <kahanalan@gmail.com>
2023-04-08 20:38:12 -0300

    agregados flags que faltaban

--- lab1_scaling.sh
+++ lab1_scaling.sh
@@ -23,7 +23,7 @@ for N in 30 32 62 64 126 128 157 254 256 401 510 512 1022 1024 1777 2046 2048; d
 
 	# Ejecuto dentro de dir
 	cd $pardir/$dir
-	./run2.sh aocc "-Ofast -march=native" "$N 0.1 0 0 5 100"
+	./run2.sh aocc "-std=c17 -Wall -Wextra -Ofast -march=native" "$N 0.1 0 0 5 100"
 	cd ../.. 		
 		
 done
