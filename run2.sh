6fbb2d9a7fc1e5e409855fba3df09c0709d1003d
alan_kahan <kahanalan@gmail.com>
2023-04-08 21:27:46 -0300

    correccion error en run2.sh

--- run2.sh
+++ run2.sh
@@ -4,8 +4,9 @@ export headless_name=headless
 export CC=$1
 export CFLAGS=$2
 
-rm ./headless.dat 
+#rm ./headless.dat 
 
-perf stat -r 10 -x ';' -o output.txt -e cpu-clock,task-clock,context-switches,page-faults,cycles,instructions,branches,faults,migrations,duration_time,cache-misses ./headless $3 >/dev/null 2>&1
+
+perf stat -r 10 -x ';' -o output.txt -e cpu-clock,task-clock,context-switches,page-faults,cycles,instructions,branches,faults,migrations,duration_time,cache-misses ./headless $3 0.1 0 0 5 100 >/dev/null 2>&1
 python3 mean_outputs.py output.txt
 cat output.txt
