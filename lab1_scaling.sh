74c4bdf3761529d06b693148ea0dd44b11850460
alan_kahan <kahanalan@gmail.com>
2023-04-08 21:13:45 -0300

    pruebas scaling

--- lab1_scaling.sh
+++ lab1_scaling.sh
@@ -14,16 +14,14 @@ for N in 30 32 62 64 126 128 157 254 256 401 510 512 1022 1024 1777 2046 2048; d
 	dir=datos_$N
 	mkdir $pardir/$dir
 
-	make clean >/dev/null 2>&1
-	make all >/dev/null 2>&1
-
 	# muevo el ejecutable al nuevo directorio
-	cp ./run2.sh $pardir/$dir/	
+	cp ./run2.sh $pardir/$dir/
+	cp ./mean_outputs.py $pardir/$dir/
 	cp ./${headless_name} $pardir/$dir/${headless_name}
 
 	# Ejecuto dentro de dir
 	cd $pardir/$dir
-	./run2.sh aocc "-std=c17 -Wall -Wextra -Ofast -march=native" "$N 0.1 0 0 5 100"
+	./run2.sh aocc "-std=c17 -Wall -Wextra -Ofast -march=native" " ${N} 0.1 0 0 5 100"
 	cd ../.. 		
 		
 done
