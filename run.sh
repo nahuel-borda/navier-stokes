bdc7fb83a21e18c7f105bab0f7c86cbc192ba235
Nahuel B <bordanah@gmail.com>
2023-05-09 19:22:33 -0300

    automations

--- run.sh
+++ run.sh
@@ -5,7 +5,18 @@ export CC=$1
 export CFLAGS=$2
 
 make clean >/dev/null 2>&1
-make all >/dev/null 2>&1
 
-perf stat -x ';' -o output.txt -e cpu-clock,task-clock,context-switches,page-faults,cycles,instructions,branches,faults,migrations,duration_time ./headless >/dev/null 2>&1
-cat output.txt
+rm -f ./headless.dat 
+rm -f vect_output.txt
+rm -f output.txt
+rm -f out.txt
+
+make headless >vectorization_report.txt 2>&1
+
+grep -c "optimized" vectorization_report.txt | xargs -I {} echo "{};;total_vectorizations;" >> vect_output.txt
+grep -c "missed" vectorization_report.txt | xargs -I {} echo "{};;missed_vectorizations;" >> vect_output.txt
+
+perf stat -r 10 -x ';' -o output.txt -e cpu-clock,task-clock,context-switches,page-faults,cycles,instructions,branches,faults,migrations,duration_time,cache-misses ./headless >/dev/null 2>&1
+python3 mean_outputs.py output.txt 
+cat output.txt vect_output.txt > out.txt
+cat out.txt
