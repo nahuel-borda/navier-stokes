9a853de91c9760e44c6d404fa956490468c7bac1
CP2023 <cp202303@debian-BULLSEYE-live-builder-AMD64>
2023-06-04 00:08:44 -0300

    simplified run_omp.sh

--- run_omp.sh
+++ run_omp.sh
@@ -6,41 +6,18 @@ export CFLAGS=$2
 
 make clean >/dev/null 2>&1
 
-rm -f headless.dat 
-touch headless.dat
-rm -f vect_output.txt
+rm -f ./headless.dat 
 rm -f output.txt
 rm -f out.txt
 rm -f headless.optrpt
 
-make all >vectorization_report.txt 2>&1
-
-if [ $CC == 'clang' ]; then
-  cat vectorization_report.txt | sort --unique | grep -c 'vectorization width' | xargs -I {} echo "{};;total_vectorizations;" >> vect_output.txt
-  cat vectorization_report.txt | sort --unique | grep -c 'pass-missed' | xargs -I {} echo "{};;missed_vectorizations;" >> vect_output.txt
-elif [ $CC == 'aocc' ]; then
-  cat vectorization_report.txt | sort --unique | grep -c 'vectorization width' | xargs -I {} echo "{};;total_vectorizations;" >> vect_output.txt
-  cat vectorization_report.txt | sort --unique | grep -c 'pass-missed' | xargs -I {} echo "{};;missed_vectorizations;" >> vect_output.txt
-elif [ $CC == 'icc' ]; then
-  cat headless.optrpt | sort --unique | grep -c 'WAS VECTORIZED' | xargs -I {} echo "{};;total_vectorizations;" >> vect_output.txt
-  cat headless.optrpt | sort --unique | grep -c 'not vectorized' | xargs -I {} echo "{};;missed_vectorizations;" >> vect_output.txt
-elif [ $CC == 'icx' ]; then
-  cat headless.optrpt | sort --unique | grep -c 'WAS VECTORIZED' | xargs -I {} echo "{};;total_vectorizations;" >> vect_output.txt
-  cat headless.optrpt | sort --unique | grep -c 'not vectorized' | xargs -I {} echo "{};;missed_vectorizations;" >> vect_output.txt
-else
-  grep -c "optimized" vectorization_report.txt | xargs -I {} echo "{};;total_vectorizations;" >> vect_output.txt
-  grep -c "missed" vectorization_report.txt | xargs -I {} echo "{};;missed_vectorizations;" >> vect_output.txt
-fi
+make all 2>&1
 
 export OMP_NUM_THREADS=$5
 
-if [ $# == 2 ]; then
-	perf stat -r 1 -x ';' -o output.txt -e cpu-clock,task-clock,context-switches,page-faults,cycles,instructions,branches,faults,migrations,duration_time,cache-misses ./headless >/dev/null 2>&1
-	python3 mean_outputs.py output.txt
-else
-	perf stat -r 1 -x ';' -o output.txt -e cpu-clock,task-clock,context-switches,page-faults,cycles,instructions,branches,faults,migrations,duration_time,cache-misses ./headless $3 0.1 0 0 5 100 $4 >/dev/null 2>&1  
-	python3 mean_outputs.py output.txt
-fi
+perf stat -r 1 -x ';' -o output.txt -e cpu-clock,task-clock,context-switches,page-faults,cycles,instructions,branches,faults,migrations,duration_time,cache-misses ./headless $3 0.1 0 0 5 100 $4
+python3 mean_outputs.py output.txt
 
-cat output.txt vect_output.txt > out.txt
+
+cat output.txt > out.txt
 cat out.txt
